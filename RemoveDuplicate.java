4662dfa568e5f9b8d33f0a216c97f9052f3da2ad
Jayanth Prajin.N <kit28.24bcs065@gmail.com>
2026-06-29 14:29:25 +0530

    Add files via upload

---
+++ RemoveDuplicate.java
@@ -0,0 +1,20 @@
+public class RemoveDuplicate {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5};
+
+        int i = 0;
+
+        for (int j = 1; j < arr.length; j++) {
+            if (arr[i] != arr[j]) {
+                i++;
+                arr[i] = arr[j];
+            }
+        }
+
+        for (int k = 0; k <= i; k++) {
+            System.out.print(arr[k] + " ");
+        }
+    }
+}
