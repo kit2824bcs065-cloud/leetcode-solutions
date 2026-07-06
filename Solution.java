834666e6f841c61c2dcfa86d972eb1bf1dad3465
Jayanth Prajin.N <kit28.24bcs065@gmail.com>
2026-07-06 11:55:31 +0530

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,55 +1,43 @@
-import java.util.Scanner;
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
 
 public class Solution {
 
-    public String minWindow(String s, String t) {
-        if (s == null || t == null || s.length() == 0 || t.length() == 0 ||
-                s.length() < t.length()) {
-            return "";
+    public ListNode middleNode(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
         }
 
-        int[] map = new int[128];
-        int count = t.length();
-        int start = 0, end = 0, minLen = Integer.MAX_VALUE, startIndex = 0;
+        ListNode slow = head;
+        ListNode fast = head;
 
-        for (char c : t.toCharArray()) {
-            map[c]++;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
 
-        char[] chS = s.toCharArray();
-
-        while (end < chS.length) {
-            if (map[chS[end++]]-- > 0) {
-                count--;
-            }
-
-            while (count == 0) {
-                if (end - start < minLen) {
-                    startIndex = start;
-                    minLen = end - start;
-                }
-
-                if (map[chS[start++]]++ == 0) {
-                    count++;
-                }
-            }
-        }
-
-        return minLen == Integer.MAX_VALUE ? "" : new String(chS, startIndex, minLen);
+        return slow;
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Solution sol = new Solution();
 
-        System.out.print("Enter string s: ");
-        String s = sc.nextLine();
+        // Create linked list: 1 -> 2 -> 3 -> 4 -> 5
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
 
-        System.out.print("Enter string t: ");
-        String t = sc.nextLine();
+        ListNode middle = sol.middleNode(head);
 
-        Solution obj = new Solution();
-        System.out.println("Minimum Window: " + obj.minWindow(s, t));
-
-        sc.close();
+        System.out.println("Middle Node = " + middle.val);
     }
 }
