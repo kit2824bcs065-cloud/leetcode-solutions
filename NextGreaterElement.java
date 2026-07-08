27e8877cc03b5ff75d42b4984914d94fbfb2c3a1
Jayanth Prajin.N <kit28.24bcs065@gmail.com>
2026-07-08 11:06:42 +0530

    Add files via upload

---
+++ NextGreaterElement.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class NextGreaterElement {
+
+    public static int[] nextGreater(int[] arr) {
+        int n = arr.length;
+        int[] result = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            while (!stack.isEmpty() && stack.peek() <= arr[i]) {
+                stack.pop();
+            }
+
+            if (stack.isEmpty()) {
+                result[i] = -1;
+            } else {
+                result[i] = stack.peek();
+            }
+
+            stack.push(arr[i]);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {4, 5, 2, 10};
+
+        int[] ans = nextGreater(arr);
+
+        System.out.println("Input Array : " + Arrays.toString(arr));
+        System.out.println("Next Greater Elements : " + Arrays.toString(ans));
+    }
+}
